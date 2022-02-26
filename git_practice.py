da8ca82289aa4594195a1955133cc7f70d5a50e5
dcmotocorp <41550974+dcmotocorp@users.noreply.github.com>
2022-02-26 11:22:40 +0530

    Update git_practice.py

--- git_practice.py
+++ git_practice.py
@@ -3,26 +3,7 @@ from django.db.models.functions import Coalesce
 
 
 class manana(models.Manager):
-    commenr
-    def with_counts(self):
-        comments 
-        return reopjhf ijn 
-    dfjf l
-     df;;sjopf 
-        fdjf 
-        self.annotate(
-            comnet
-            num_responses=Coalesce(models.Count("response"), 0)
-        )
-
-
-class OpinionPoll(models.Model):
-    question = models.CharField(max_length=200)
-    objects = PollManager()
-
+    PASS 
     
-class Rename(models.Model):
-    qudffdfon = models.CharField(max_length=200)
-    objectsddsfsdfs = REname()
-
+ 
  
