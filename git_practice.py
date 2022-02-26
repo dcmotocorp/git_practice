87490800fd4ffebc286be0426388adc6b57597ab
name <name@gmail.com>
2022-02-26 10:58:10 +0530

    Add file practice to  folter

---
+++ git_practice.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.db.models.functions import Coalesce
+
+
+class manana(models.Manager):
+    def with_counts(self):
+        return self.annotate(
+            num_responses=Coalesce(models.Count("response"), 0)
+        )
+
+
+class OpinionPoll(models.Model):
+    question = models.CharField(max_length=200)
+    objects = PollManager()
